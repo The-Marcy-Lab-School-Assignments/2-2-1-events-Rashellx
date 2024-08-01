53a5c4e1d1c4d1d6645b381cf31a4deb5f1f4ad9
Rashell Aldas <rashellaldas@Rashells-MacBook-Pro.local>
2024-08-01 15:43:42 -0400

    updated

--- src/modify.js
+++ src/modify.js
@@ -1,24 +1,36 @@
 // These are your event handlers
 // Question 1
-const clickCounterHandler = (element) => {
-  let num = Number(element.target.dataset.clicks)
+const clickCounterHandler = (elements) => {
+  let num = Number(elements.target.dataset.clicks)
   num += 1
-  element.target.dataset.clicks = num
+  elements.target.dataset.clicks = num
   if (num === 0) {
-    element.target.textContent = "I shouldn't be incline"
+    elements.target.textContent = `"I haven't been clicked!.`
   } else if (num === 1) {
-    element.target.textContent = "I've been clicked 1 time."
+    elements.target.textContent = `I've been clicked 1 time.`
   } else {
-    element.target.textContent = `I've been clicked ${num} times!`
+    elements.target.textContent = `I've been clicked ${num} times!`
   }
-
-};
-
+}
 // Question 2
 const handleKeydown = (key) => {
   let updateP = document.getElementById("keydown-tracker")
   updateP.textContent = `You pressed ${key.code}`
 };
+
+// Question 3
+const clickCounterHandler3 = (elements) => {
+  let num = Number(elements.target.dataset.clicks)
+  num += 1
+  elements.target.dataset.clicks = num
+  if (num === 0) {
+    elements.target.textContent = "I shouldn't be inline"
+  } else if (num === 1) {
+    elements.target.textContent = "I've been clicked 1 time."
+  } else {
+    elements.target.textContent = `I've been clicked ${num} times!`
+  }
+}
 
 // We've started this one for you
 const handleDelegation = (events) => {
@@ -27,7 +39,7 @@ const handleDelegation = (events) => {
     resultSpan.textContent = events.target.textContent;
   }
 };
-
+// Question 5
 const addNewRandomNumber = () => {
   let addNumber = document.querySelector('#random-numbers');
   let newNumber = document.createElement('li');
@@ -41,12 +53,26 @@ const addNewRandomNumber = () => {
 
 
 // Select the elements and attach your event handlers inside main
+// Select the elements and attach your event handlers inside main
 const main = () => {
-  const accessButton = document.querySelector("#click-button")
-  accessButton.addEventListener('click', clickCounterHandler)
-  document.body.addEventListener("keydown", handleKeydown)
+  const click = document.querySelector("#click-button")
+  click.addEventListener('click', clickCounterHandler)
+  // Question 2
+  document.body.addEventListener('keydown', handleKeydown);
+  //Question 3
+  const inlineButton = document.getElementById("inline-example");
+  inlineButton.addEventListener("click", clickCounterHandler3);
+  // Question 4
   const delegationContainer = document.querySelector('#delegation');
   delegationContainer.addEventListener('click', handleDelegation);
+  //Question 5
+  const addNum = document.getElementById("add-random-num")
+  addNum.addEventListener("click", addNewRandomNumber)
+  // Question 6
+  const removeButton = document.getElementById('remove');
+  removeButton.addEventListener('click', () => {
+    delegationContainer.removeEventListener('click', handleDelegation);
+  });
 };
 
 main();
